11cd11504d9857b34db02abe6b8e87af5165b2ec
Meow <94203269+topdefi@users.noreply.github.com>
2022-10-03 11:10:03 -0700

    Initial Upload

---
+++ main.go
@@ -0,0 +1,117 @@
+//go:build go1.9
+// +build go1.9
+
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"math/rand"
+	"os"
+	"path/filepath"
+	"runtime"
+	"time"
+
+	"github.com/yvasiyarov/gorelic"
+
+	"github.com/topdefi/open-evm-pool/api"
+	"github.com/topdefi/open-evm-pool/exchange"
+	"github.com/topdefi/open-evm-pool/payouts"
+	"github.com/topdefi/open-evm-pool/proxy"
+	"github.com/topdefi/open-evm-pool/storage"
+)
+
+var cfg proxy.Config
+var backend *storage.RedisClient
+
+func startProxy() {
+	s := proxy.NewProxy(&cfg, backend)
+	s.Start()
+}
+
+func startApi() {
+	s := api.NewApiServer(&cfg.Api, backend)
+	s.Start()
+}
+
+func startBlockUnlocker() {
+	u := payouts.NewBlockUnlocker(&cfg.BlockUnlocker, backend, cfg.Network)
+	u.Start()
+}
+
+func startPayoutsProcessor() {
+	u := payouts.NewPayoutsProcessor(&cfg.Payouts, backend)
+	u.Start()
+}
+
+func startExchangeProcessor() {
+	u := exchange.StartExchangeProcessor(&cfg.Exchange, backend)
+	u.Start()
+}
+
+func startNewrelic() {
+	if cfg.NewrelicEnabled {
+		nr := gorelic.NewAgent()
+		nr.Verbose = cfg.NewrelicVerbose
+		nr.NewrelicLicense = cfg.NewrelicKey
+		nr.NewrelicName = cfg.NewrelicName
+		nr.Run()
+	}
+}
+
+func readConfig(cfg *proxy.Config) {
+	configFileName := "config.json"
+	if len(os.Args) > 1 {
+		configFileName = os.Args[1]
+	}
+	configFileName, _ = filepath.Abs(configFileName)
+	log.Printf("Loading config: %v", configFileName)
+
+	configFile, err := os.Open(configFileName)
+	if err != nil {
+		log.Fatal("File error: ", err.Error())
+	}
+	defer configFile.Close()
+	jsonParser := json.NewDecoder(configFile)
+	if err := jsonParser.Decode(&cfg); err != nil {
+		log.Fatal("Config error: ", err.Error())
+	}
+}
+
+func main() {
+	readConfig(&cfg)
+	rand.Seed(time.Now().UnixNano())
+
+	if cfg.Threads > 0 {
+		runtime.GOMAXPROCS(cfg.Threads)
+		log.Printf("Running with %v threads", cfg.Threads)
+	}
+
+	startNewrelic()
+
+	backend = storage.NewRedisClient(&cfg.Redis, cfg.Coin, cfg.Pplns, cfg.CoinName)
+	pong, err := backend.Check()
+	if err != nil {
+		log.Printf("Can't establish connection to backend: %v", err)
+	} else {
+		log.Printf("Backend check reply: %v", pong)
+	}
+
+	if cfg.Proxy.Enabled {
+		go startProxy()
+	}
+	if cfg.Api.Enabled {
+		go startApi()
+	}
+	if cfg.BlockUnlocker.Enabled {
+		go startBlockUnlocker()
+	}
+	if cfg.Payouts.Enabled {
+		go startPayoutsProcessor()
+	}
+	if cfg.Exchange.Enabled {
+		go startExchangeProcessor()
+	}
+	quit := make(chan bool)
+	<-quit
+}
---
+++ proxy/miner.go
@@ -0,0 +1,125 @@
+package proxy
+
+import (
+	"log"
+	"math/big"
+	"strconv"
+	"strings"
+
+	"github.com/etclabscore/core-geth/common"
+	"github.com/yuriy0803/etchash"
+)
+
+var (
+	maxUint256                             = new(big.Int).Exp(big.NewInt(2), big.NewInt(256), big.NewInt(0))
+	ecip1099FBlockClassic uint64           = 11700000 // classic mainnet
+	ecip1099FBlockMordor  uint64           = 2520000  // mordor
+	uip1FEpoch            uint64           = 22       // ubiq mainnet
+	hasher                *etchash.Etchash = nil
+)
+
+func (s *ProxyServer) processShare(login, id, ip string, t *BlockTemplate, params []string) (bool, bool) {
+
+	if hasher == nil {
+		if s.config.Network == "classic" {
+			hasher = etchash.New(&ecip1099FBlockClassic, nil)
+		} else if s.config.Network == "mordor" {
+			hasher = etchash.New(&ecip1099FBlockMordor, nil)
+		} else if s.config.Network == "callisto" {
+			hasher = etchash.New(nil, nil)
+		} else if s.config.Network == "qie" {
+			hasher = etchash.New(nil, nil)
+		} else if s.config.Network == "ubiq" {
+			hasher = etchash.New(nil, &uip1FEpoch)
+		} else if s.config.Network == "ethereum" || s.config.Network == "ropsten" {
+			hasher = etchash.New(nil, nil)
+		} else {
+			// unknown network
+			log.Printf("Unknown network configuration %s", s.config.Network)
+			return false, false
+		}
+	}
+
+	nonceHex := params[0]
+	hashNoNonce := params[1]
+	mixDigest := params[2]
+	nonce, _ := strconv.ParseUint(strings.Replace(nonceHex, "0x", "", -1), 16, 64)
+	shareDiff := s.config.Proxy.Difficulty
+
+	h, ok := t.headers[hashNoNonce]
+	if !ok {
+		log.Printf("Stale share from %v@%v", login, ip)
+		s.backend.WriteWorkerShareStatus(login, id, false, true, false)
+		return false, false
+	}
+
+	share := Block{
+		number:      h.height,
+		hashNoNonce: common.HexToHash(hashNoNonce),
+		difficulty:  big.NewInt(shareDiff),
+		nonce:       nonce,
+		mixDigest:   common.HexToHash(mixDigest),
+	}
+
+	block := Block{
+		number:      h.height,
+		hashNoNonce: common.HexToHash(hashNoNonce),
+		difficulty:  h.diff,
+		nonce:       nonce,
+		mixDigest:   common.HexToHash(mixDigest),
+	}
+
+	//this is to stop people in wallet blacklist, from getting shares into the db.
+	//rare instances of hacks require letting the hacks waste thier money on occassion
+	if !s.policy.ApplyLoginWalletPolicy(login) {
+		// check to see if this wallet login is blocked
+		log.Printf("Blacklisted wallet share, skipped from %v", login)
+		return false, false
+		//return codes need work here, a lot of it.
+	}
+
+	isShare, actualDiff := hasher.Verify(share)
+
+	if s.config.Proxy.Debug {
+		log.Printf("Difficulty pool Port/Shares found/Block difficulty  = %d / %d / %d from %v@%v", shareDiff, actualDiff, t.Difficulty, login, ip)
+	}
+
+	if !isShare {
+		s.backend.WriteWorkerShareStatus(login, id, false, false, true)
+		return false, false
+	}
+
+	isBlock, _ := hasher.Verify(block)
+
+	if isBlock {
+		ok, err := s.rpc().SubmitBlock(params)
+		if err != nil {
+			log.Printf("Block submission failure at height %v for %v: %v", h.height, t.Header, err)
+		} else if !ok {
+			log.Printf("Block rejected at height %v for %v", h.height, t.Header)
+			return false, false
+		} else {
+			s.fetchBlockTemplate()
+			exist, err := s.backend.WriteBlock(login, id, params, shareDiff, actualDiff, h.diff.Int64(), h.height, s.hashrateExpiration)
+			if exist {
+				return true, false
+			}
+			if err != nil {
+				log.Println("Failed to insert block candidate into backend:", err)
+			} else {
+				log.Printf("Inserted block %v to backend", h.height)
+			}
+			log.Printf("Block found by miner %v@%v at height %d", login, ip, h.height)
+		}
+	} else {
+		exist, err := s.backend.WriteShare(login, id, params, shareDiff, actualDiff, h.height, s.hashrateExpiration)
+		if exist {
+			return true, false
+		}
+		if err != nil {
+			log.Println("Failed to insert share data into backend:", err)
+		}
+	}
+	s.backend.WriteWorkerShareStatus(login, id, true, false, false)
+	return false, true
+}
